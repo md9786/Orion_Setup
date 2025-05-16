f938973fe7c7f69b26f7d797bc4299a1d9710e7d
md9786 <66461507+md9786@users.noreply.github.com>
2025-05-16 15:45:39 +0330

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,6 +6,15 @@ apt-get update -y && apt-get upgrade -y
 # Function to display the main menu
 show_menu() {
     clear
+    echo " .d88888b.          d8b                         888     888 8888888b.  888b    888 "
+    echo "d88P^ ^Y88b         Y8P                         888     888 888   Y88b 8888b   888 "
+    echo "888     888                                     888     888 888    888 88888b  888 "
+    echo "888     888 888d888 888  .d88b.  88888b.        Y88b   d88P 888   d88P 888Y88b 888 "
+    echo "888     888 888P^   888 d88^^88b 888 ^88b        Y88b d88P  8888888P^  888 Y88b888 "
+    echo "888     888 888     888 888  888 888  888         Y88o88P   888        888  Y88888 "
+    echo "Y88b. .d88P 888     888 Y88..88P 888  888          Y888P    888        888   Y8888 "
+    echo " ^Y88888P^  888     888  ^Y88P^  888  888           Y8P     888        888    Y888 "
+    echo ""
     echo "╔════════════════════════════════════════════╗"
     echo "║           Server Setup Menu                ║"
     echo "╠════════════════════════════════════════════╣"
